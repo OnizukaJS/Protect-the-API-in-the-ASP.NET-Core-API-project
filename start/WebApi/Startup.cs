1fdc34bd209bb2c6d7b3327c0efca04990d174c6
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2022-11-23 17:00:11 +0100

    Protect the API in the ASP.NET Core API project

---
+++ start/WebApi/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Logging;
+using OpenIddict.Validation.AspNetCore;
+
+namespace WebApi;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+        });
+
+        // Register the OpenIddict validation components.
+        services.AddOpenIddict()
+            .AddValidation(options =>
+            {
+                // Note: the validation handler uses OpenID Connect discovery
+                // to retrieve the address of the introspection endpoint.
+                options.SetIssuer("https://localhost:44318/");
+                options.AddAudiences("rs_dataEventRecordsApi");
+
+                // Configure the validation handler to use introspection and register the client
+                // credentials used when communicating with the remote introspection endpoint.
+                //options.UseIntrospection()
+                //        .SetClientId("rs_dataEventRecordsApi")
+                //        .SetClientSecret("dataEventRecordsSecret");
+
+                // disable access token encyption for this
+                options.UseAspNetCore();
+
+                // Register the System.Net.Http integration.
+                options.UseSystemNetHttp();
+
+                // Register the ASP.NET Core host.
+                options.UseAspNetCore();
+            });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("dataEventRecordsPolicy", policyUser =>
+            {
+                policyUser.RequireClaim("scope", "dataEventRecords");
+            });
+        });
+        
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        IdentityModelEventSource.ShowPII = true;
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
